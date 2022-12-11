3857a1b043f1bb5cbdeca43f24ed448e40a3103e
Elvyn Mejia <elvynmejia@gmail.com>
2022-12-11 09:42:18 -0800

    removes console log statements

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,15 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-console.log({
-  path: path.join(__dirname, './../../', 'frontend', 'build', 'index.html')
-});
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  // res.render(
-  //   path.join(__dirname, './../../', 'frontend', 'build', 'index.html')
-  // );
+  res.send('/ end point respond with a resource');
 });
 
 module.exports = router;
